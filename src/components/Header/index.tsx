280dceeb236b3338908a71f0c72274aa43744563
Martelode38 <marteniers@gmail.com>
2024-07-23 00:21:24 -0300

    Dialog

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,12 +1,30 @@
 import { HeaderContainer, HeaderContent, NewTransactionButton } from "./styles";
 import logo from '../../assets/logo.svg';
+import * as Dialog from '@radix-ui/react-dialog';
 
 export function Header(){
     return(
         <HeaderContainer>
             <HeaderContent>
                 <img src={logo} alt="" />
-                <NewTransactionButton>Nova transação</NewTransactionButton>
+
+                <Dialog.Root>
+                    <Dialog.Trigger asChild>
+                        <NewTransactionButton>Nova transação</NewTransactionButton>
+                    </Dialog.Trigger>
+                
+
+                <Dialog.Portal>
+                    {/*o portal coloca o conteudo do modal fora de todas as divs*/}
+                    <Dialog.Overlay/>{/*coloca o fundo preto opaco atras do modal*/}
+
+                    <Dialog.Content>
+                        <Dialog.Title>Nova Transações</Dialog.Title>
+                        <Dialog.Close/>
+                    </Dialog.Content>
+                </Dialog.Portal>
+            </Dialog.Root>
+
             </HeaderContent>
         </HeaderContainer>
     );
