4eb06748f30c400b46db4e3f96cb21baf1cfaad1
Martelode38 <marteniers@gmail.com>
2024-07-24 20:24:22 -0300

    .

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,7 +1,7 @@
 import * as Dialog from "@radix-ui/react-dialog";
-import { CloseButton, Content, Overlay } from "./styles";
-import { X } from "phosphor-react";
-
+import { CloseButton, Content, Overlay, TransactionType, TransactionTypeButton } from "./styles";
+import { ArrowCircleDown, ArrowCircleUp, X } from "phosphor-react";
+// nessa eu utilizo a biblioteca radix para melhorar a cessibilidade do projeto
 export function NewTransactionModal(){
     return(
         <Dialog.Portal>
@@ -17,6 +17,19 @@ export function NewTransactionModal(){
                 <input type="text" placeholder="Descrição" required/>
                 <input type="number" placeholder="Preço" required/>
                 <input type="text" placeholder="Caregoria" required/>
+
+                <TransactionType>
+                    <TransactionTypeButton variant="income" value="income">
+                        <ArrowCircleUp size={24}/>
+                        Entrada
+                    </TransactionTypeButton>
+                    
+                    <TransactionTypeButton variant="outcome" value="outcome">
+                        <ArrowCircleDown size={24}/>
+                        Saída
+                    </TransactionTypeButton>
+
+                </TransactionType>
 
                 <button type="submit">Cadastrar</button>
             </form>
