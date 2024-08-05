e8d624bf1f3c9522d39582711323b3c716879707
Martelode38 <marteniers@gmail.com>
2024-08-04 21:25:03 -0300

    .

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -18,9 +18,9 @@ export function Transactions(){
     const [transactions, setTransactions] = useState<Transaction[]>([]);
     
     async function loadTransactions() {
-        const response = await fetch('http://localhost:5173/transactions')
+        const response = await fetch('http://localhost:3333/transactions');
         const data = await response.json();
-       
+
         setTransactions(data);
     }
     
@@ -38,15 +38,15 @@ export function Transactions(){
                 <SearchForm/>
                 <TransactionTable>
                     <tbody>
-                        {transactions.map(transactions =>{
+                        {transactions.map(transaction =>{
                             return(
-                                <tr key={transactions.id}>
-                                    <td width="50%">{transactions.description}</td>
+                                <tr key={transaction.id}>
+                                    <td width="50%">{transaction.description}</td>
                                     <td>
-                                    <PriceHighLight variant={transactions.type}>{transactions.price}</PriceHighLight>
+                                    <PriceHighLight variant={transaction.type}>{transaction.price}</PriceHighLight>
                                     </td>
-                                    <td>{transactions.category}</td>
-                                    <td>{transactions.createAt}</td>
+                                    <td>{transaction.category}</td>
+                                    <td>{transaction.createAt}</td>
                                 </tr>
                             )
                         })}
